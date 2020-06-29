3c5286dda19f34185bbe767c6295568089030e0e
Lee JaeHyun <myleje@gmail.com>
2020-06-29 14:42:40 +0900

    .

---
+++ Assets/Scripts/GrabManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabManager : MonoBehaviour
+{
+    private Transform tr;
+    private GameObject grabObject;
+    private bool isTouched = false;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("BALL"))
+        {
+            isTouched = true;
+        }
+    }
+}
