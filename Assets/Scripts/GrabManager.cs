b9c4b3b15b2d633a03801c8e21a59ebad5906547
Lee JaeHyun <myleje@gmail.com>
2020-06-29 15:08:10 +0900

    .

--- Assets/Scripts/GrabManager.cs
+++ Assets/Scripts/GrabManager.cs
@@ -27,7 +27,24 @@ public class GrabManager : MonoBehaviour
         if (isTouched == false
             && OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
         {
-            
+            joint.connectedBody = grabObject.GetComponent<Rigidbody>();
         }
+
+        if (isTouched == true
+            && OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
+        {
+            //오른쪽 컨트롤러의 속도 산출
+            Vector3 _velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+            grabObject.GetComponent<Rigidbody>().velocity = _velocity;
+            joint.connectedBody = null;
+            isTouched = false;            
+        }
+        //헵틱 효과
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            OVRInput.SetControllerVibration(0.5f, 0.5f, OVRInput.Controller.RTouch);
+        }
+
+        
     }
 }
